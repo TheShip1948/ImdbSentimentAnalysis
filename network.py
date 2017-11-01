febce197ad9bc7d60a6ae7cf3d6a2d44f548761e
bebo <beshoy@hotmail.com>
2017-10-31 23:18:48 -0700

    Simple NN implementation.

--- network.py
+++ network.py
@@ -1,40 +1,34 @@
-################################################
-# --- Imports ---
-################################################
-import numpy as np 
-from keras.datasets import imdb 
-from matplotlib import pyplot 
-
-################################################
-# --- Load data ---
-################################################
-(X_train, y_train) , (X_test, y_test) = imdb.load_data() 
-X = np.concatenate((X_train, X_test) , axis=0)
-y = np.concatenate((y_train, y_test) , axis=0) 
-
-# Summarize size 
-print("Training data: ... ")
-print(X.shape)
-print(y.shape)
-# print(X_train[0])
-# Summarize number of classes
-print("Classes: ")
-print(np.unique(y))
-# Summarize number of words
-print("Number of words: ")
-print(len(np.unique(np.hstack(X))))
-# Summarize review length
-print("Review length: ")
-result = map(len, X)
-print("Mean %.2f words (%f)" % (np.mean(result), np.std(result)))
-# plot review length as a boxplot and histogram
-pyplot.subplot(121)
-pyplot.boxplot(result)
-pyplot.subplot(122)
-pyplot.hist(result)
-pyplot.show()
-
-
+# MLP for the IMDB problem
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load the dataset but only keep the top n words, zero the rest
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
+max_words = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+# create the model
+model = Sequential()
+model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(Flatten())
+model.add(Dense(250, activation= 'relu' ))
+model.add(Dense(1, activation= 'sigmoid' ))
+model.compile(loss= 'binary_crossentropy' , optimizer= 'adam' , metrics=[ 'accuracy' ])
+print(model.summary())
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=2, batch_size=128,
+verbose=1)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
 
